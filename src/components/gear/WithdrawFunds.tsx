b69a61c47af8290180679d3aa2fa32d7a0c1d692
TerratekMusic <35459364+TerratekMusic@users.noreply.github.com>
2023-12-09 15:44:08 -0600

    gear components

---
+++ src/components/gear/WithdrawFunds.tsx
@@ -0,0 +1,7 @@
+import { Text } from "@chakra-ui/react";
+
+function WithdrawFunds() {
+  return <Text>HOlamundo</Text>;
+}
+
+export { WithdrawFunds };
--- src/pages/dapp/Dapp.tsx
+++ src/pages/dapp/Dapp.tsx
@@ -7,100 +7,17 @@ import {
   web3FromSource,
 } from "@polkadot/extension-dapp";
 import { useAccount, useAlert } from "@gear-js/react-hooks";
-import { Button, Input, Modal } from "@gear-js/ui";
+
+import { DepositFunds } from "components/gear/DepositFunds";
 
 function Dapp() {
-  const { account } = useAccount();
-
-  const [isModalOpen, setIsModalOpen] = useState(false);
-
-  const openModal = () => {
-    setIsModalOpen(true);
-  };
-
-  const closeModal = () => {
-    setIsModalOpen(false);
-  };
-
-  const [address, setAddress] = useState("");
-
-  const [valueAmount, setValueAmount] = useState("");
-
-  const alert = useAlert();
-
-  const wsEndpoint = "wss://testnet.vara-network.io";
-
-  const transfer = async function main() {
-    await web3Enable("my wallet");
-
-    const allAccounts = await web3Accounts();
-
-    const accountextension = allAccounts[0];
-
-    const wsProvider = new WsProvider(wsEndpoint);
-
-    const api = await ApiPromise.create({ provider: wsProvider });
-
-    const transferExtrinsic = api.tx.balances.transfer(
-      address,
-      Number(valueAmount) * 1000000000000
-    );
-
-    setValueAmount("");
-    setAddress("");
-
-    const injector = await web3FromSource(accountextension.meta.source);
-
-    transferExtrinsic
-      .signAndSend(
-        accountextension.address,
-        { signer: injector.signer },
-        ({ status }) => {
-          if (status.type === "Finalized") {
-            alert.success(status.type);
-          }
-        }
-      )
-      .catch((error: any) => {
-        console.log(":( transaction failed", error);
-      });
-  };
-
-  const AddressInputChange = (event: any) => {
-    setAddress(event.target.value);
-  };
-
-  const AmountInputChange = (event: any) => {
-    setValueAmount(event.target.value);
-  };
   return (
     <Container
       justifyContent="center"
       bgGradient="linear(to-l, #121215 ,#284736)"
       minWidth="100vw"
     >
-      <Button text="Transfer" onClick={openModal} />
-      {isModalOpen && (
-        <Modal size="large" heading="Connect" close={closeModal}>
-          <Center>
-            <Heading size="xs" textColor="white">
-              Address
-            </Heading>
-          </Center>
-          <Input type="text" value={address} onChange={AddressInputChange} />
-          <p>{address}</p>
-          <Center>
-            <Heading size="xs" textColor="white">
-              Amount
-            </Heading>
-          </Center>
-          <Input type="text" value={valueAmount} onChange={AmountInputChange} />
-          <p>{valueAmount}</p>
-          <Center>
-            <Button text="Send" onClick={() => transfer()} />
-          </Center>
-        </Modal>
-      )}
+      <DepositFunds />
     </Container>
   );
 }
