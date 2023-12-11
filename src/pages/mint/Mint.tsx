4ce2c787a5bf4d48ed01eaffb4c231f0422360f8
TerratekMusic <35459364+TerratekMusic@users.noreply.github.com>
2023-12-11 17:01:44 -0600

    Mint Page1

---
+++ src/pages/mint/Mint.tsx
@@ -0,0 +1,61 @@
+import { useAccount, useApi, useAlert } from "@gear-js/react-hooks";
+import { web3FromSource } from "@polkadot/extension-dapp";
+import { ProgramMetadata } from "@gear-js/api";
+import { Button } from "@gear-js/ui";
+
+function Mint() {
+  const alert = useAlert();
+  const { accounts, account } = useAccount();
+  const { api } = useApi();
+
+  // Add your programID
+  const programIDFT = " Add your programID";
+
+  // Add your metadata.txt
+  const meta = "Add your metadata.txt";
+
+  const metadata = ProgramMetadata.from(meta);
+
+  const message: any = {
+    destination: programIDFT, // programId
+    payload: { mint: 1000 },
+    gasLimit: 899819245,
+    value: 0,
+  };
+
+  const signer = async () => {
+    const localaccount = account?.address;
+    const isVisibleAccount = accounts.some(
+      (visibleAccount) => visibleAccount.address === localaccount
+    );
+
+    if (isVisibleAccount) {
+      // Create a message extrinsic
+      const transferExtrinsic = await api.message.send(message, metadata);
+
+      const injector = await web3FromSource(accounts[0].meta.source);
+
+      transferExtrinsic
+        .signAndSend(
+          account?.address ?? alert.error("No account"),
+          { signer: injector.signer },
+          ({ status }) => {
+            if (status.isInBlock) {
+              alert.success(status.asInBlock.toString());
+            } else if (status.type === "Finalized") {
+              alert.success(status.type);
+            }
+          }
+        )
+        .catch((error: any) => {
+          console.log(":( transaction failed", error);
+        });
+    } else {
+      alert.error("Account not available to sign");
+    }
+  };
+
+  return <Button text="Mint" onClick={signer} />;
+}
+
+export { Mint };
